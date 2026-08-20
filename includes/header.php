50cb21e7a8547c38e0e15c1de77b0f7c21cd38b8
ZH4RK <ritzlarano@gmail.com>
2026-08-20 16:30:48 +0800

    Add favicon link to header for improved branding

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@
 
     <title>DTI XII Sports Fest</title>
 
+    <link rel="icon" type="image/png" href="assets/images/logo.png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="assets/css/style.css" rel="stylesheet">
